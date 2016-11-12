f5e95f663faa6cbbaf8d061b9fa0e4c0b77ecf96
Michael Montgomery <montgomery.michael@hotmail.com>
2016-11-11 22:28:42 -0500

    addition of capitalizeFirst exercise

---
+++ capitalizeFirst/capitalizeFirst.js
@@ -0,0 +1,15 @@
+var capitalizeFirst = function(str) {
+    if(str.split(' ').length > 1) {
+        str = str.split(' ')[0]
+    }
+    var holder = [];
+    var stringAsArray = str.split('');
+    stringAsArray.forEach(function(v, i, a) {
+        if(i === 0) {
+            holder.push(v.toUpperCase())
+        } else {
+            holder.push(v.toLowerCase())
+        }
+    });
+    return holder.join('')
+};
