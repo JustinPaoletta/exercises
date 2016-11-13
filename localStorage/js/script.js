971757eee29baa5cb1ff2d41bfcb0de1020608ea
Michael Montgomery <montgomery.michael@hotmail.com>
2016-11-12 19:20:21 -0500

    added functionality to control visibility of clear button based on input in email field

--- localStorage/js/script.js
+++ localStorage/js/script.js
@@ -2,13 +2,21 @@ var checkForAndInsertStorage = function () {
     if(localStorage.getItem('email')) {
         document.getElementById('email').placeholder = '';
         document.getElementById('email').value = localStorage.getItem('email');
+        document.getElementById('clear').style.display = 'inline';
     }
 };
 
 checkForAndInsertStorage();
 
+
+
 document.getElementById('email').addEventListener('keyup', function() {
     localStorage.setItem('email', document.getElementById('email').value);
+    if(document.getElementById('email').value.length > 0) {
+        document.getElementById('clear').style.display = 'inline';
+    } else {
+        document.getElementById('clear').style.display = 'none';
+    }
 });
 
 document.getElementById('clear').addEventListener('click', function() {
