717e2d4a98d38493861d7657fd52e4104c9cbf6c
Michael Montgomery <montgomery.michael@hotmail.com>
2017-02-15 16:51:51 -0500

    refactored lotterPicker exercise.

--- lotteryPicker/picker.js
+++ lotteryPicker/picker.js
@@ -1,60 +1,21 @@
-var numberRun = function(inMax, inHowMany) {
+var numberRun = function(max, howManyNumbers) {
+    var holder = [];
     var count = {};
-
-    var recounter = 0;
-
-
-    var numbers = [];
-    var numbersTwo = [];
-    var finalArr = [];
-    var seriousArr = [];
-
-    for(var i = 0; i < inHowMany; i++) {
-        numbers.push(Math.floor((Math.random() * inMax) + 1))
+    var dupe = false;
+    for(var i = 0; i < howManyNumbers; i++) {
+        holder.push(Math.floor((Math.random() * max) + 1))
     }
-
-    numbers.forEach(function(val) {
-        numbersTwo.push(val);
-    });
-
-    numbers.forEach(function(val) {
-        if(count[val.toString()]) {
-            count[val.toString()] += 1
-        } else {
-            count[val.toString()] = 1
+    holder.forEach(function(val, idx, arr) {
+        for(var x = 0; x < holder.length; x++) {
+            if(idx === x) {
+                console.log('same index')
+            } else if(val === holder[x]) {
+                dupe = true;
+            }
         }
     });
-
-    for(key in count) {
-        if(count[key] > 1) {
-            delete count[key]
-        } else {
-            finalArr.push(key)
-        }
+    if(dupe) {
+        numberRun(max, howManyNumbers)
     }
-
-    var difference = (inHowMany - finalArr.length);
-
-    if(difference != 0) {
-        recounter++;
-        for(var i = 0; i < difference; i++) {
-            finalArr.push(Math.floor((Math.random() * inMax) + 1))
-        }
-    }
-
-    finalArr.forEach(function(val) {
-        if(typeof val === "string") {
-            val = Number(val);
-        }
-    });
-
-
-
-    finalArr.forEach(function(val) {
-        seriousArr.push(Number(val))
-    });
-
-    return finalArr.sort();
-
-}
-
+    return holder.sort()
+};
